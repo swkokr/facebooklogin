63210da2f6f4ef30c12d1bbcbe88ae23936d3112
rexxxxxx <haroo0310@gmail.com>
2016-08-10 12:06:23 +0900

    GPS checker in MainActivity

--- app/src/main/java/com/example/swko/facebooktest/MainActivity.java
+++ app/src/main/java/com/example/swko/facebooktest/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.swko.facebooktest;
 
+import android.Manifest;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
@@ -9,12 +15,45 @@ import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private GpsInfo gps;
+    private Button gps_btn;
+    private TextView gps_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //commit test Kwook
         AppEventsLogger.activateApp(this);
+
+        gps_btn = (Button) findViewById(R.id.gps_btn);
+        gps_txt = (TextView) findViewById(R.id.gps_txt);
+
+
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        // GPS 정보를 보여주기 위한 이벤트 클래스 등록
+        gps_btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                gps = new GpsInfo(MainActivity.this);
+                // GPS 사용유무 가져오기
+                if (gps.isGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    gps_txt.setText(String.valueOf(latitude)+String.valueOf(longitude));
+
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "당신의 위치 - \n위도: " + latitude + "\n경도: " + longitude,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    // GPS 를 사용할수 없으므로
+                    gps.showSettingsAlert();
+                }
+            }
+        });
     }
 }
